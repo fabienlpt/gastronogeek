710b4304a204ac7ad0066f8e2eaa0e307484e56c
Fabien <lapertfabien@gmail.com>
2024-11-29 17:36:59 +0100

    feature(app): update colors

--- src/app/utils/transitionOverlay.tsx
+++ src/app/utils/transitionOverlay.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const TransitionOverlay = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div className="fixed inset-0 z-50 pointer-events-none overflow-hidden">
-      <div className="absolute inset-0 bg-black transform-gpu" ref={ref}>
+      <div className="absolute inset-0 bg-[#0F172A] transform-gpu" ref={ref}>
         <div className="absolute inset-0 flex items-center justify-center text-white text-2xl font-bold">
           <Image
             src="/logo.png"
